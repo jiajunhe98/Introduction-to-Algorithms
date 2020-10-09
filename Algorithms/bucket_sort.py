c76333cb2a2a8a9f885a780bf54eb64270d4c4fa
Jiajun He <43494544+hejj16@users.noreply.github.com>
2020-10-09 09:28:50 +0200

    Update bucket_sort.py

--- Algorithms/bucket_sort.py
+++ Algorithms/bucket_sort.py
@@ -16,17 +16,4 @@ def bucket_sort(list):
             result.append(j)
     return result
 
-#test
-a = [0.1,0.2,0.3,0.2,0.111,0.3,0.2,0.5,0.3,0.53,0.8,0.57,0.85,0.67,0.99]
-print(bucket_sort(a))
-print("")
 
-b = []
-import random
-for i in range(int(1e5)):
-    b.append(random.random())
-import time
-start = time.time()
-newb = bucket_sort(b)
-end = time.time()
-print ("time:"+ str(end -start))
